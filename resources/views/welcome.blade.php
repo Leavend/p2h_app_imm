41e0d2c7fcc93597ec8df54f014bbf9fd5790c3b
XXFRXX <112834783+XXFRXX@users.noreply.github.com>
2023-08-10 07:30:09 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,7 +129,24 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <!-- Start Pagination -->
+              <div class='pagination-container'>
+                  <nav>
+                      <ul class="pagination">
+                          <li data-page="prev">
+                              <span> <
+                                  <span class="sr-only">(current)
+                              </span></span>
+                          </li>
+          <!-- Here the JS Function Will Add the Rows -->
+                          <li data-page="next" id="prev">
+                              <span> > <span class="sr-only">(current)</span></span>
+                          </li>
+                      </ul>
+                  </nav>
+              </div>
                         {{-- <div class="pagination">
+        
                             {{ $kendaraanData->links() }}
                         </div> --}}
                     </section>
