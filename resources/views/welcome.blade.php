ebd96994e56d0b31bde0a27a466590232011066f
XXFRXX <112834783+XXFRXX@users.noreply.github.com>
2023-08-10 07:28:12 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,6 +124,21 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <div class='pagination-container'>
+                  <nav>
+                      <ul class="pagination">
+                          <li data-page="prev">
+                              <span> <
+                                  <span class="sr-only">(current)
+                              </span></span>
+                          </li>
+          <!-- Here the JS Function Will Add the Rows -->
+                          <li data-page="next" id="prev">
+                              <span> > <span class="sr-only">(current)</span></span>
+                          </li>
+                      </ul>
+                  </nav>
+              </div>
                     </section>
         </main>
     </div>
