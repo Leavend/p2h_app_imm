71a5d42de1cd98ecfee30995847d4c727e3e22d9
XXFRXX <112834783+XXFRXX@users.noreply.github.com>
2023-08-14 22:01:34 +0800

    Update show.blade.php

--- resources/views/p2h/show.blade.php
+++ resources/views/p2h/show.blade.php
@@ -185,9 +185,8 @@
                                                         <span class="note">{{ $jamCarbon->format('g:i A') }}</span>
                                                     </td>
                                                     <td style="display: flex; justify-content: center">
-                                                        <a href="{{ url('admin/p2h/detail/' . $x->id) }}"
-                                                            class="btn btn-circle btn-info"><i
-                                                                class="fas fa-eye"></i></a>
+                                                        <a><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+														View</button></a>
                                                         <a href="{{ url('admin/p2h/edit/' . $x->id) }}"
                                                             class="btn btn-circle btn-warning"><i
                                                                 class="fas fa-edit"></i></a>
@@ -561,5 +560,22 @@
     </div><!--//app-wrapper-->
 
     @include('layoutDashboard.js')
-
+<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+	  	<div class="modal-content">
+			<div class="modal-header">
+		  		<h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+		  		<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+		  	...
+			</div>
+			<div class="modal-footer">
+		  		<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  		<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+	  	</div>
+		</div>
+  	</div>
 </body>
