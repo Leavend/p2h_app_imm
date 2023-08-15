99e0b0f38527f2b6dc0fb388657f4bca7c9fcff9
Leavend <tiohadybayu@gmail.com>
2023-08-15 23:48:39 +0800

    commit several update

---
+++ deploy-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigasi ke direktori repositori Anda
+cd /var/www/p2h_app_imm
+
+# Lakukan git pull
+git pull origin Version2  # Ganti "main" dengan nama cabang yang sesuai
+
+# (Opsional) Restart layanan web server Anda (misalnya, Nginx atau Apache)
+sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
+
+echo "Git pull completed successfully"
