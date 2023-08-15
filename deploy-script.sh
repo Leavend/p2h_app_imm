9565ba976a7ff9012624e4e4d9c14e3420a5b5f5
Leavend <tiohadybayu@gmail.com>
2023-08-16 00:16:02 +0800

    commit several update

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,8 +3,10 @@
 # Navigasi ke direktori repositori Anda
 # cd /var/www/p2h_app_imm/
 
+git config --global --add safe.directory /var/www/p2h_app_imm
+
 # Lakukan git pull dengan mengatur konfigurasi safe.directory
-git -c safe.directory=/var/www/p2h_app_imm pull --rebase origin Version2
+git pull --rebase origin Version2
 
 # (Opsional) Restart layanan web server Anda (misalnya, Nginx atau Apache)
 sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
