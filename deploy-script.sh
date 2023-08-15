009178c01eafa180d61b049abe957ba032a52269
Leavend <tiohadybayu@gmail.com>
2023-08-16 01:08:37 +0800

    commit several update

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,11 +1,21 @@
-set e
+#!/bin/bash
 
-sudo sudo git commit -m 'stash'
+# Pastikan script dijalankan sebagai root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# Navigasi ke direktori repositori Anda
+cd /var/www/p2h_app_***/
+
+# Lakukan commit dengan memberi pesan "stash"
+sudo git commit -m 'stash'
 
 # Lakukan git pull dengan mengatur konfigurasi safe.directory
-sudo sudo git pull --rebase origin Version2
+sudo git pull --rebase origin Version2  # Ganti "main" dengan nama cabang yang sesuai
 
 # (Opsional) Restart layanan web server Anda (misalnya, Nginx atau Apache)
-sudo sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
+sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
 
 echo "Git pull completed successfully"
