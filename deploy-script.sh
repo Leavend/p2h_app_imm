731b8c3b9da9ca4d931da0ca487f1ba1eed39b63
Leavend <tiohadybayu@gmail.com>
2023-08-16 01:06:54 +0800

    commit several update

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,11 +1,11 @@
 set e
 
-sudo git commit -m 'stash'
+sudo sudo git commit -m 'stash'
 
 # Lakukan git pull dengan mengatur konfigurasi safe.directory
-sudo git pull --rebase origin Version2
+sudo sudo git pull --rebase origin Version2
 
 # (Opsional) Restart layanan web server Anda (misalnya, Nginx atau Apache)
-sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
+sudo sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
 
 echo "Git pull completed successfully"
--- resources/views/layoutLanding/footer.blade.php
+++ resources/views/layoutLanding/footer.blade.php
@@ -1,11 +1,11 @@
     <!-- Footer-->
-    {{-- <footer class="bg-light py-5">
+    <footer class="bg-light py-5">
         <div class="container px-4 px-lg-5">
             <div class="small text-center text-muted">
                 Copyright &copy; 2023 - Indominco Mandiri - General Affairs
             </div>
         </div>
-    </footer> --}}
+    </footer>
 
 
     <!-- Bootstrap core JS-->
