f07548797eebaada4d4668869ec87970f4df34dd
Leavend <tiohadybayu@gmail.com>
2023-08-16 01:05:39 +0800

    commit several update

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,9 +1,9 @@
 set e
 
-git commit -m 'stash'
+sudo git commit -m 'stash'
 
 # Lakukan git pull dengan mengatur konfigurasi safe.directory
-git pull --rebase origin Version2
+sudo git pull --rebase origin Version2
 
 # (Opsional) Restart layanan web server Anda (misalnya, Nginx atau Apache)
 sudo systemctl restart nginx  # Ganti "nginx" dengan nama layanan web server Anda
