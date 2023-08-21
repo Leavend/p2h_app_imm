996f8b7ca488635a5e0b59d4eb61b943ea296970
Leavend <tiohadybayu@gmail.com>
2023-08-21 08:58:10 +0800

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,8 @@
             </div>
         </div>
         <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
-            <div class="auth-background-holder"></div>
+            <div class="auth-background-holder">
+            </div>
             <div class="auth-background-mask"></div>
             <div class="auth-background-overlay p-3 p-lg-5">
                 <div class="d-flex flex-column align-content-end h-100">
