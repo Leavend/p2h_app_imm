df548e5afe214358b2c30c963e188b01922e7bf0
Leavend <tiohadybayu@gmail.com>
2023-08-21 09:00:27 +0800

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                         </a>
                     </div>
                     <h2 class="auth-heading text-center mb-5">Log in to Dashboard P2H</h2>
-                    <div class="auth-form-container text-start">
+                    {{-- <div class="auth-form-container text-start">
                         <form class="auth-form login-form" action="{{ route('auth.login') }}" method="POST">
                             @csrf
                             <div class="mb-3">
@@ -32,7 +32,7 @@
                                     In</button>
                             </div>
                         </form>
-                    </div>
+                    </div> --}}
                 </div>
                 @include('layoutDashboard.footer')
             </div>
