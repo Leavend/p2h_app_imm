891407f97d7c6c93eff817ea2f138e0108e11a2a
Leavend <tiohadybayu@gmail.com>
2023-08-23 20:40:46 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,20 +11,19 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
-        // Jalankan event CreateDailyP2hEntries setiap hari pada pukul 01:00
-        $schedule->call(function () {
-            event(new CreateDailyP2hEntries());
-        })->dailyAt('01:00')->timezone('Asia/Makassar');
+        $schedule->event(new CreateDailyP2hEntries())->dailyAt('01:00')->timezone('Asia/Makassar');
     }
+
 
     /**
      * Register the commands for the application.
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__ . '/Commands');
+
         require base_path('routes/console.php');
     }
 }
