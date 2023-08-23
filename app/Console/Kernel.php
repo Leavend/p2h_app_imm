87dd1002adba460322d34c5e33640ffd2036434f
Leavend <tiohadybayu@gmail.com>
2023-08-24 07:54:37 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('p2h:run-create-daily-p2h-entries')->dailyAt('01:00')->timezone('Asia/Makassar');
+        $schedule->command('p2h:run-create-daily-p2h-entries')->dailyAt('08:00')->timezone('Asia/Makassar');
     }
 
 
