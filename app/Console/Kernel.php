da5556ce8db9aa2a4435a4115340ec128400e1cd
Leavend <tiohadybayu@gmail.com>
2023-08-24 09:18:27 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,10 @@
 
 namespace App\Console;
 
-use App\Events\CreateDailyP2hEntries;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Kendaraan;
+use App\Models\P2h;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,17 +14,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('p2h:run-create-daily-p2h-entries')->dailyAt('01:00')->timezone('Asia/Makassar');
-    }
+        $schedule->call(function () {
+            $kendaraans = Kendaraan::all();
 
-
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
-        $this->load(__DIR__ . '/Commands');
-
-        require base_path('routes/console.php');
+            foreach ($kendaraans as $kendaraan) {
+                $p2h = new P2h([
+                    'kendaraan_id' => $kendaraan->id,
+                    'status' => 'menunggu verifikasi',
+                    'tanggal' => now('Asia/Makassar')->format('Y-m-d'),
+                    'jam' => now('Asia/Makassar')->format('H:i:s'),
+                ]);
+                $p2h->save();
+            }
+        })->dailyAt('09:30')->timezone('Asia/Makassar');
     }
 }
