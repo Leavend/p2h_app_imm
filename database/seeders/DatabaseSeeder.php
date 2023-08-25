deb6b9da39359fc223f2a3aadcc33b133cb28376
Leavend <tiohadybayu@gmail.com>
2023-08-25 15:23:39 +0800

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // P2h::factory()->count(20)->create();
+        P2h::factory()->count(20)->create();
         // User::factory()->count(20)->create();
-        Kendaraan::factory()->count(20)->create();
+        // Kendaraan::factory()->count(20)->create();
     }
 }
