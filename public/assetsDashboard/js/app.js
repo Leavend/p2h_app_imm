b47084600a28681e6f99ad9419e8387be29fa676
XXFRXX <112834783+XXFRXX@users.noreply.github.com>
2023-08-31 13:50:59 +0800

    Update app.js

--- public/assetsDashboard/js/app.js
+++ public/assetsDashboard/js/app.js
@@ -19,6 +19,8 @@ const sidePanelToggler = document.getElementById('sidepanel-toggler');
 const sidePanel = document.getElementById('app-sidepanel');  
 const sidePanelDrop = document.getElementById('sidepanel-drop'); 
 const sidePanelClose = document.getElementById('sidepanel-close'); 
+const maincontent = document.getElementById('main-content');
+const sidePanelinner = document.getElementById('resp');
 
 window.addEventListener('load', function(){
 	responsiveSidePanel(); 
@@ -36,12 +38,18 @@ function responsiveSidePanel() {
 	    //console.log('larger');
 		sidePanel.classList.remove('sidepanel-hidden');
 		sidePanel.classList.add('sidepanel-visible');
+		sidePanel.classList.remove('responsive');
+		maincontent.classList.remove('responsive');
+		sidePanelinner.classList.remove('responsive');
 		
 	} else {
 	    // if smaller
 	    //console.log('smaller');
 	    sidePanel.classList.remove('sidepanel-visible');
 		sidePanel.classList.add('sidepanel-hidden');
+		sidePanel.classList.add('responsive');
+		maincontent.classList.add('responsive');
+		sidePanelinner.classList.add('responsive');
 	}
 };
 
@@ -49,7 +57,8 @@ sidePanelToggler.addEventListener('click', () => {
 	if (sidePanel.classList.contains('sidepanel-visible')) {
 		console.log('visible');
 		sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
+		sidePanel.classList.add('sidepanel-hidden');	
+
 		
 	} else {
 		console.log('hidden');
@@ -57,6 +66,7 @@ sidePanelToggler.addEventListener('click', () => {
 		sidePanel.classList.add('sidepanel-visible');
 	}
 });
+
 
 
 
@@ -68,6 +78,7 @@ sidePanelClose.addEventListener('click', (e) => {
 sidePanelDrop.addEventListener('click', (e) => {
 	sidePanelToggler.click();
 });
+
 
 
 /* ====== Mobile search ======= */
@@ -88,7 +99,71 @@ searchMobileTrigger.addEventListener('click', () => {
 		searchMobileTriggerIcon.classList.add('fa-magnifying-glass');
 	}
 	
+		
+	
 });
+
+// Function to handle checkbox selection for a row
+function handleCheckboxSelection(rowId) {
+	const checkboxes = document.querySelectorAll(`#${rowId} .form-check-input`);
+	checkboxes.forEach(checkbox => {
+		checkbox.addEventListener('change', () => {
+			checkboxes.forEach(cb => {
+				if (cb !== checkbox) {
+					cb.checked = false;
+				}
+			});
+		});
+	});
+}
+
+// asas //
+
+const header = document.getElementById('header-content');
+const Toggler = document.getElementById('sidepanel-toggler'); 
+const content = document.getElementById('main-content')
+const respon = document.getElementById('brand-resp')
+const navresp = document.getElementById('nav-resp');
+const appresp = document.getElementById('app-nav-main')
+const appnav = document.getElementById('menu-accordion')
+
+Toggler.onclick = function(){
+	header.classList.toggle('active');
+	content.classList.toggle('active');
+	respon.classList.toggle('responsive');
+	navresp.classList.toggle('responsive');
+	appresp.classList.toggle('responsive');
+	appnav.classList.toggle('responsive');
+
+	// Toggle the class for nav-icon as well
+    const navIcons = document.querySelectorAll('.app-nav .nav-icon');
+    navIcons.forEach(navIcon => {
+        navIcon.classList.toggle('responsive');
+    });
+
+	const navLink = document.querySelectorAll('.app-nav .nav-link');
+    navLink.forEach(navLink => {
+        navLink.classList.toggle('responsive');
+    });
+
+	const navText = document.querySelectorAll('.app-nav .nav-link-text');
+    navText.forEach(navText => {
+        navText.classList.toggle('responsive');
+    });
+	const sideApp = document.querySelectorAll('sidepanel-inner');
+    sideApp.forEach(sideApp => {
+        sideApp.classList.toggle('responsive');
+    });
+
+	// const maincontent = document.querySelectorAll('.app-wrapper');
+    // maincontent.forEach(maincontent => {
+    //     maincontent.classList.toggle('responsive');
+    // });
+
+	
+	
+	
+}
 
 
 
